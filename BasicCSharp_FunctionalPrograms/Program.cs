22e043d5e04c377f29ead962041c5a5f0e57c800
khushbu <khushbusoni475@gmail.com>
2023-05-22 17:11:18 +0530

    [khushbu] Adding HelloWorld

---
+++ BasicCSharp_FunctionalPrograms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCSharp_FunctionalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome HelloWorld");
+            Console.ReadLine();
+        }
+    }
+}
